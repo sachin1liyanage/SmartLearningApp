371d61bcc5358d2ade023a429434b020099f33c4
sachin1liyanage <mora4d@gmail.com>
2018-06-17 13:36:29 +0530

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {    
+  apiKey: "AIzaSyARqXu4OprUxQD6ySWVWpLERV4AwdMNUd8",
+  authDomain: "angularjsapp-b52a8.firebaseapp.com",
+  databaseURL: "https://angularjsapp-b52a8.firebaseio.com",
+  projectId: "angularjsapp-b52a8",
+  storageBucket: "angularjsapp-b52a8.appspot.com",
+  messagingSenderId: "248460811650"   
+  }
 };
 
 /*

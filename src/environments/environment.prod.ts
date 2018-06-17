e1bfe982b6c5145245c526779584c05ba001b423
sachin1liyanage <mora4d@gmail.com>
2018-06-17 22:21:18 +0530

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase: {    
+    apiKey: 'AIzaSyARqXu4OprUxQD6ySWVWpLERV4AwdMNUd8',
+    authDomain: 'angularjsapp-b52a8.firebaseapp.com',
+    databaseURL: 'https://angularjsapp-b52a8.firebaseio.com',
+    projectId: 'angularjsapp-b52a8',
+    storageBucket: 'angularjsapp-b52a8.appspot.com',
+    messagingSenderId: '248460811650'   
+    }
 };
